a93e0ced3b7e02768367e9cb8022eda59b91af3d
francisco cuesta <cuestafrancisco@gmail.com>
2020-11-28 22:46:44 -0500

    update description in index.html

---
+++ build/precache-manifest.6efb96cca5566fcf527cf2dfdf4b0c29.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe2648cb028ee804c97732eff177dad8",
+    "url": "/webDevPortfolio/index.html"
+  },
+  {
+    "revision": "a72f9a22edfb682c153a",
+    "url": "/webDevPortfolio/static/css/2.f1201ff7.chunk.css"
+  },
+  {
+    "revision": "37cea02457f7893745c8",
+    "url": "/webDevPortfolio/static/css/main.1455f7f9.chunk.css"
+  },
+  {
+    "revision": "a72f9a22edfb682c153a",
+    "url": "/webDevPortfolio/static/js/2.448dd0be.chunk.js"
+  },
+  {
+    "revision": "b71cb3cccc73e099d412c24a04bf9105",
+    "url": "/webDevPortfolio/static/js/2.448dd0be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "37cea02457f7893745c8",
+    "url": "/webDevPortfolio/static/js/main.e08d6f5e.chunk.js"
+  },
+  {
+    "revision": "f557537c7c6b7ef7ebce",
+    "url": "/webDevPortfolio/static/js/runtime-main.30610f2f.js"
+  },
+  {
+    "revision": "13cbc420a3975483557030deaaa26fd4",
+    "url": "/webDevPortfolio/static/media/resume.13cbc420.pdf"
+  }
+]);
